9da40e52b899161d911baa99c581f8fa269935f6
Muawia24 <amuawia666@gmail.com>
2024-12-15 23:56:07 +0200

    Tested mongoDB connection

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import process from 'process';
+import cors from 'cors';
+import { Server } from 'socket.io';
+import http from 'http';
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors)
+
+console.log(process.env.PORT);
+console.log(process.env.MONGO_URI);
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => console.log('MongoDB connected!'))
+.catch((err) => console.log(err));
+
+app.get('/', (req, res) => {
+    res.status(200).send('Backend running...')
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Listening to port: ${PORT}`);
+});
